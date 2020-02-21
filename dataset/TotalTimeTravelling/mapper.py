c57024fb81b382e0793b5657792554ca157cff3a
ganesh58 <srpganesh10@gmail.com>
2020-02-21 17:45:48 -0600

    ganesh mapper and reducer

---
+++ dataset/TotalTimeTravelling/mapper.py
@@ -0,0 +1,11 @@
+input_mapper = open("data.txt","r")
+output_mapper = open("output1.txt","w")
+for line in input_mapper:
+    data = line.strip().split(',')
+    if (len(data) == 12):
+       
+        trip_id, start_time, end_time, bikeid, tripduration, from_station_id, from_station_name, to_station_id, to_station_name, usertype, gender, birthyear = data
+
+        output_mapper.write(from_station_name+","+tripduration+"\n")
+input_mapper.close()
+output_mapper.close()
---
+++ dataset/TotalTimeTravelling/reducer.py
@@ -0,0 +1,17 @@
+s = open("output1.txt","r")
+r = open("output2", "w")
+
+thisKey = ""
+thisValue = 0.0
+for line in s:
+  data = line.strip().split(',')
+  from_station_name, tripduration = data
+  if from_station_name != thisKey:
+    if thisKey:
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+    thisKey = from_station_name 
+    thisValue = 0.0
+  thisValue += float(tripduration)
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+s.close()
+r.close()
